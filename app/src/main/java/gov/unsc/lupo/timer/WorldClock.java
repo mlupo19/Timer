8fc01328a223451befbe719c43253e02cb1ff673
D_Diamonds <laxisgood4u@gmail.com>
2020-01-30 18:48:22 -0500

    httpconnection

--- app/src/main/java/gov/unsc/lupo/timer/WorldClock.java
+++ app/src/main/java/gov/unsc/lupo/timer/WorldClock.java
@@ -2,12 +2,53 @@ package gov.unsc.lupo.timer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class WorldClock extends Activity {
+
+    public void sendGetRequest(String link) {
+        try {
+            URL url = new URL(link);
+            HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
+            httpConnection.setRequestMethod("GET");
+            httpConnection.setInstanceFollowRedirects(false);
+
+            int responseCode = httpConnection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(httpConnection.getInputStream()));
+                StringBuffer response = new StringBuffer();
+                // print result
+                System.out.println("JSON String Result " + response.toString());
+                //GetAndPost.POSTRequest(response.toString());
+            } else {
+                System.out.println("GET NOT WORKED");
+            }
+
+        }
+        catch (IOException e) {
+            Log.e("IOException", e.toString());
+        }
+    }
+
+    public void parseJSON(String JSON) {
+        JSONParser
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_world_clock);
+        String baseURL = "http://api.timezonedb.com/v2.1/get-time-zone?key=EJEFWNO52XKX&format=json&by=position";
+        String lat = "&lat=40.689247";
+        String lon = "&lng=-74.044502";
+        parseJSON(sendGetRequest(baseURL + lat + lon));
+
+
     }
 }
