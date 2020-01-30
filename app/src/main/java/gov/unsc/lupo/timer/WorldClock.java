83559d9b4548731621de092a89c451eb91073add
mlupo <mike.lupo19@gmail.com>
2020-01-30 08:14:49 -0500

    Initial commit

---
+++ app/src/main/java/gov/unsc/lupo/timer/WorldClock.java
@@ -0,0 +1,14 @@
+package gov.unsc.lupo.timer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorldClock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_world_clock);
+    }
+}
