3dde9a34152351355ad28f695adfd79f67e7d242
Mike <mike.lupo19@gmail.com>
2020-02-03 23:23:21 -0500

    josh bad at lua

--- app/src/main/java/gov/unsc/lupo/timer/Timer.java
+++ app/src/main/java/gov/unsc/lupo/timer/Timer.java
@@ -9,12 +9,16 @@ import android.widget.TextView;
 
 public class Timer extends Activity {
 
+    private EditText etHours, etMinutes, etSeconds;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
 
-
+        etHours = findViewById(R.id.tEditHours);
+        etMinutes = findViewById(R.id.tEditMinutes);
+        etSeconds = findViewById(R.id.tEditSeconds);
     }
 
     public void editClick(View v) {
@@ -22,6 +26,21 @@ public class Timer extends Activity {
     }
 
     public void startClick(View v) {
+        if (!numberIsValid())
+            return;
+
+        etHours.setEnabled(false);
+        etMinutes.setEnabled(false);
+        etSeconds.setEnabled(false);
+
+
+    }
+
+    private boolean numberIsValid() {
+        return false;
+    }
+
+    public void resetClick(View v) {
 
     }
 
