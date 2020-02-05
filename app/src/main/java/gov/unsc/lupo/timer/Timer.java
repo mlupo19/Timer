bf3a776d9448c572edd3cccbf646fad9541926db
mlupo <mike.lupo19@gmail.com>
2020-02-05 08:26:57 -0500

    timer working

--- app/src/main/java/gov/unsc/lupo/timer/Timer.java
+++ app/src/main/java/gov/unsc/lupo/timer/Timer.java
@@ -7,9 +7,14 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class Timer extends Activity {
 
     private EditText etHours, etMinutes, etSeconds;
+    private int hours, mins, seconds;
+    private boolean running;
+    private CountDownTimer cdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,26 +27,79 @@ public class Timer extends Activity {
     }
 
     public void editClick(View v) {
-        ((EditText) v).setText("");
+        if (v.isEnabled())
+            ((EditText) v).setText("");
     }
 
     public void startClick(View v) {
-        if (!numberIsValid())
+        if (!numberIsValid() || running)
             return;
 
         etHours.setEnabled(false);
         etMinutes.setEnabled(false);
         etSeconds.setEnabled(false);
 
+        running = true;
+        startTimer();
+    }
 
+    private void startTimer() {
+        int time = (hours * 3600) + (mins * 60) + seconds;
+        cdt = new CountDownTimer(time * 1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                updateTimer((int) (millisUntilFinished / 1000));
+            }
+
+            @Override
+            public void onFinish() {
+                running = false;
+            }
+        };
+        cdt.start();
+    }
+
+    private void updateTimer(int currentTime) {
+        if (!running)
+            return;
+        seconds = currentTime % 60;
+        currentTime /= 60;
+        mins = currentTime % 60;
+        currentTime /= 60;
+        hours = currentTime;
+
+        etHours.setText(String.format(Locale.US, "%02d", hours));
+        etMinutes.setText(String.format(Locale.US, "%02d", mins));
+        etSeconds.setText(String.format(Locale.US, "%02d", seconds));
     }
 
     private boolean numberIsValid() {
-        return false;
+        try {
+            hours = Integer.parseInt(etHours.getText().toString());
+            mins = Integer.parseInt(etMinutes.getText().toString());
+            seconds = Integer.parseInt(etSeconds.getText().toString());
+
+            if (seconds >= 60 || mins >= 60 || hours < 0 || mins < 0 || seconds < 0) {
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
     public void resetClick(View v) {
+        cdt.cancel();
+        running = false;
 
+        etHours.setText("00");
+        etMinutes.setText("00");
+        etSeconds.setText("00");
+
+        etHours.setEnabled(true);
+        etMinutes.setEnabled(true);
+        etSeconds.setEnabled(true);
     }
 
 }
